ddf9e26da8618cfa94c141684e1ad1314037cc3b
mlohithraj <rlohith767@gmail.com>
2024-02-22 08:57:01 +0530

    Making a form and consoling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -41,6 +42,10 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  const addMovieHandler = (movie) => {
+    console.log(movie)
+  }
 
   const startRetry = () => {
     const intervalId = setInterval(() => {
@@ -84,6 +89,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie = {addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
