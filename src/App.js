8d6ad9a6e7a0bb8996b96f073782a2a3b0312aa8
mlohithraj <rlohith767@gmail.com>
2024-02-27 08:10:14 +0530

    Adding logout issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useContext } from 'react';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './components/store/auth-context';
 
 function App() {
+  const autCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
+        {!autCtx.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
         <Route path="/profile">
-          <UserProfile />
+          {autCtx.isLoggedIn && <UserProfile />}
+          {!autCtx.isLoggedIn && <Redirect to='/auth' />}
+          </Route>
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -8,26 +8,33 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem('token');
   const [token, setToken] = useState(null);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
   };
 
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
-    logout: logoutHandler
-  }
+    logout: logoutHandler,
+  };
 
-  return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
-export default AuthContext
+export default AuthContext;
