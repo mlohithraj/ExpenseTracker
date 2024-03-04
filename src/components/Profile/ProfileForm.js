823a20d0738ad65c4d97c226d57e466b054e8917
mlohithraj <rlohith767@gmail.com>
2024-03-04 07:26:29 +0530

    Editing the user details. When the user updated the profile and refreshes all details are gone.

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,47 @@
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useEffect, useState } from 'react';
 import classes from './ProfileForm.module.css';
 import AuthContext from '../store/auth-context';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const ProfileForm = () => {
   const authCtx = useContext(AuthContext);
   const nameInputRef = useRef();
   const photoInputRef = useRef();
+  const [initialName, setInitialName] = useState('');
+  const [initialPhotoUrl, setInitialPhotoUrl] = useState('');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const idToken = authCtx.token;
+      const apiUrl = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyAyA-q0e_kzrsBi07QnurND_HsTyTaiZBw`;
+      const requestBody = {
+        idToken: idToken,
+      };
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          body: JSON.stringify(requestBody),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch user data');
+        }
+
+        const data = await response.json();
+        const userData = data.users[0];
+        setInitialName(userData.displayName || '');
+        setInitialPhotoUrl(userData.photoUrl || '');
+      } catch (error) {
+        console.error('Error fetching user data:', error.message);
+      }
+    };
+
+    fetchUserData();
+  }, [authCtx.token]);
 
   const updateHandler = async (event) => {
     event.preventDefault();
@@ -47,15 +83,29 @@ const ProfileForm = () => {
     <form className={classes.form} onSubmit={updateHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Full Name</label>
-        <input type="text" id="name" required ref={nameInputRef} />
+        <input
+          type="text"
+          id="name"
+          required
+          ref={nameInputRef}
+          defaultValue={initialName}
+        />
       </div>
       <div className={classes.control}>
         <label>Profile Photo Url</label>
-        <input type="text" id="photoUrl" required ref={photoInputRef} />
+        <input
+          type="text"
+          id="photoUrl"
+          required
+          ref={photoInputRef}
+          defaultValue={initialPhotoUrl}
+        />
       </div>
-      <button type="submit" className={classes.action}>
-        Update
-      </button>
+      <Link to="/profile">
+        <button type="submit" className={classes.action}>
+          Update
+        </button>
+      </Link>
     </form>
   );
 };
