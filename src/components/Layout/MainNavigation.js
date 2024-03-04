3b020e79ca1a32e074eba87603d0a6edb95ff6cf
mlohithraj <rlohith767@gmail.com>
2024-03-04 20:15:23 +0530

    Implementing logout handler

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,27 +1,43 @@
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import { useContext } from 'react';
+import AuthContext from '../store/auth-context';
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>Expense Tracker</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <Link to='/verifyAccount'>Verify Account</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/verifyAccount">Verify Account</Link>
+            </li>
+          )}
+          {isLoggedIn && (<li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>)}
         </ul>
       </nav>
     </header>
--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,20 +1,28 @@
+import { useContext } from 'react';
 import classes from './StartingPageContent.module.css';
 import { Link } from 'react-router-dom';
+import AuthContext from '../store/auth-context';
 
 const StartingPageContent = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
   return (
     <section className={classes.starting}>
       <h1>Welcome To Expense Tracker !!!</h1>
-      <div className={classes.actions}>
-        <p>
-          Your Profile is Incomplete
-          <Link to="/profile">
-            <button type="button" className={classes.toggle}>
-              Complete Profile
-            </button>
-          </Link>
-        </p>
-      </div>
+      {isLoggedIn && (
+        <div className={classes.actions}>
+          <p>
+            Your Profile is Incomplete
+            <Link to="/profile">
+              <button type="button" className={classes.toggle}>
+                Complete Profile
+              </button>
+            </Link>
+          </p>
+        </div>
+      )}
     </section>
   );
 };
